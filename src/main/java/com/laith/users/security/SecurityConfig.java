5e05c058691424846e421edf4c9270aa457086d0
Laith Mahdi <109853134+LaithMahdi@users.noreply.github.com>
2023-05-13 02:19:54 +0100

    adding same configuration

--- src/main/java/com/laith/users/security/SecurityConfig.java
+++ src/main/java/com/laith/users/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.laith.users.security;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +39,23 @@ public class SecurityConfig {
 	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	 http.csrf().disable().sessionManagement()
 	 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+	 .cors().configurationSource(new CorsConfigurationSource() {
+		 @Override
+		 public CorsConfiguration getCorsConfiguration(HttpServletRequest
+		request) {
+		 CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		 config.setAllowedMethods(Collections.singletonList("*"));
+		 config.setAllowCredentials(true);
+		 config.setAllowedHeaders(Collections.singletonList("*"));
+		 config.setExposedHeaders(Arrays.asList("Authorization"));
+		 config.setMaxAge(3600L);
+		 return config;
+		 }
+		 }).and()
+	 
+
 	 .authorizeHttpRequests().requestMatchers("/login").permitAll()
 	 .requestMatchers("/all").hasAuthority("ADMIN")
 	 .anyRequest().authenticated().and()
